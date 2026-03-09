e00c60096da5be2b00eda300741dca972820ac9a
Ivona Astafeva <maguczka@gmail.com>
2026-03-09 12:59:31 +0300

    Fix Pylint errors

--- hw6.py
+++ hw6.py
@@ -1,14 +1,20 @@
+"""
+Homework 6
+"""
 def replace():
+    """Changes symbol # to / in strings."""
     url = 'www.my_site.com#about'
     print(url.replace("#", "/"))
 replace()
 
 def ing():
+    """Adds -ing to words."""
     word = input("Enter your word: ")
     print(word + 'ing')
 ing()
 
 def name_switch():
+    """Switches places of name and surname."""
     name = 'Ivanou Ivan'
     arr = name.split( )
     arr_inverted = (arr[1], arr[0])
@@ -16,16 +22,20 @@ def name_switch():
 name_switch()
 
 def remove_spaces():
+    """Removes spaces before and after the user's input."""
     text = input("Enter some text with spaces: ")
     print(text.strip())
 remove_spaces()
 
+
 def capitalize():
+    """Capitalizing the word."""
     proper_name = 'pARiS'
     print(proper_name.capitalize())
 capitalize()
 
 def split_string():
+    """Splits strings into words."""
     str1 = 'Robin Singh'
     print(str1.split())
     str2 = 'I love arrays they are my favorite'
@@ -34,6 +44,7 @@ split_string()
 
 
 def welcome_message():
+    """Prints welcome message."""
     arr = ['Robin', 'Singh']
     str1 = 'Welcome'
     str2 = 'airport'
@@ -41,16 +52,17 @@ def welcome_message():
 welcome_message()
 
 
-
 def arr_to_string():
+    """Converts a list to string."""
     arr = ["I", "love", "arrays", "they", "are", "my", "favorite"]
     print(" ".join(arr))
 arr_to_string()
 
 
 def list_formatting():
-    list = input(('Enter 10 characters separated by spaces: ').strip()).split()
-    list[2] = input('Enter new value for the 3rd character: ').strip()
-    del list[6]
-    print(list)
+    """Formats the 3rd symbol of the user's input, and deletes the user's symbol of index [6]"""
+    users_list = input(('Enter 10 characters separated by spaces: ').strip()).split()
+    users_list[2] = input('Enter new value for the 3rd character: ').strip()
+    del users_list[6]
+    print(users_list)
 list_formatting()
