00d84c658e0d69d590b5a5c34248c6e4187800ea
Ivona Astafeva <maguczka@gmail.com>
2026-03-20 20:16:57 +0300

    Fix the comments, move the number generation into a function

--- hw8/cows.py
+++ hw8/cows.py
@@ -7,17 +7,26 @@
 Цель - отгадать число.
 """
 
+
 import random
 
-generated_number = str(random.randint(1000, 9999))  # pylint: disable=invalid-name
-while len(set(generated_number)) != 4:
-    generated_number = str(random.randint(1000, 9999))  # pylint: disable=invalid-name
+def number_generation():
+    """Генерирует 4-значное число с уникальными числами"""
 
+    generator_list = list(range(10))
+    random.shuffle(generator_list)
+    if generator_list[0] == 0:
+        generator_list[0] = generator_list[4]
+    return "".join(map(str, generator_list[:4]))
+
+generated_number = number_generation()
 print("Компьютер загадал 4-значное число из уникальных цифр.")
 
 while True:
     users_try = input('Твой вариант: ')
-
+    if not users_try.isdigit() or len(set(users_try)) != 4 or len(users_try) != 4:
+        print('Ошибка. Введите 4-значное число с уникальными цифрами..')
+        continue
     bulls = 0  # pylint: disable=invalid-name
     cows = 0  # pylint: disable=invalid-name
 
@@ -26,7 +35,6 @@ while True:
             bulls += 1
         elif users_try[i] in generated_number:
             cows += 1
-
     print(f"Быков: {bulls}, Коров: {cows}")
 
     if bulls == 4:
