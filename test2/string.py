4cbaf7e85443002ba90c4ea0a7c63afa83955b2c
Ivona Astafeva <maguczka@gmail.com>
2026-04-16 19:19:05 +0300

    Task 1

---
+++ test2/string.py
@@ -0,0 +1,8 @@
+given_string = "qwertyuiopasdfghjkl"
+print(given_string[0])
+print(given_string[-1])
+print(given_string[4])
+print(given_string[-3])
+print(len(given_string))
+print(given_string[::-1])
+print(given_string[0:7])
